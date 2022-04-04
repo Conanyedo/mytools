26361741f0e3aac018ba465eed29b02e59907c3d
Younes Bouddou <ybouddou@e2r8p15.1337.ma>
2022-04-04 00:43:20 +0000

    adding reset option

--- reset.sh
+++ reset.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo "matb9ach tkhli session m7lola hhh" > ~/.zsh_history
+mv ~/Library/Application\ Support/Knowledge/.TIG/.lmaktab/* ~/Desktop
+mv ~/Library/Application\ Support/Knowledge/.TIG/.ta7milat/* ~/Downloads
+rm -rf ~/Library/Application\ Support/Knowledge/.TIG
 rm -rf ~/Library/LaunchAgents/com.local.hh.plist
 hidutil property --set '{"UserKeyMapping":[]}'
+echo "matb9ach tkhli session m7lola hhh" > ~/.zsh_history
